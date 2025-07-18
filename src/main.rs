380d9ee0c350f237efab2ff291976d8eb067e19d
Mardiprk <mk1659770@gmail.com>
2025-07-18 15:36:54 +0530

    actix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{HttpServer, Responder, get, App};
+
+#[get("/")]
+async fn hello() -> impl Responder{
+    "Hello, Actix Web!"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()>{
+    println!("Starting Server at http://127.0.0.1:8080");
+
+    HttpServer::new(||{
+        App::new().service(hello)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
