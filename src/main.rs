bb81ffbea237d053d8c12b9313789aedb72505ef
Mardiprk <mk1659770@gmail.com>
2025-07-18 17:34:43 +0530

    actix

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,38 @@
-use actix_web::{HttpServer, Responder, get, App};
+use actix_web::{HttpServer, Responder, get, App, web, post, HttpResponse};
+use serde::{Deserialize};
+
+#[derive(Deserialize)]
+struct GreetRequest{
+    name: String
+}
 
 #[get("/")]
-async fn hello() -> impl Responder{
-    "Hello, Actix Web!"
+async fn home() -> impl Responder{
+    "Homepage"
 }
+
+#[get("/hello/{name}")]
+async fn hello(name: web::Path<String>) -> impl Responder{
+    format!("Hello, {name}")
+}
+
+#[post("/greet")]
+async fn greet(req: web::Json<GreetRequest>) -> impl Responder {
+    let name = &req.name;
+    HttpResponse::Ok().body(format!("Hello, {name}!"))
+}
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()>{
-    println!("Starting Server at http://127.0.0.1:8080");
+    println!("Starting Server http://127.0.0.1:8080");
 
     HttpServer::new(||{
-        App::new().service(hello)
-    })
-    .bind(("127.0.0.1", 8080))?
+        App::new()
+        .service(home)
+        .service(greet)
+        .service(hello)
+    }).bind(("127.0.0.1",8080))?
     .run()
     .await
 }
